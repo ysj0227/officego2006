f9d6c3cf82d71d98f2292e4948f83a4efe036709
yangshijie <1976290043@qq.com>
2020-08-29 11:14:36 +0800

    add

--- owner/src/main/java/com/owner/utils/UnIdifyDialog.java
+++ owner/src/main/java/com/owner/utils/UnIdifyDialog.java
@@ -2,7 +2,6 @@ package com.owner.utils;
 
 import android.content.Context;
 
-import com.officego.commonlib.utils.log.LogCat;
 import com.officego.commonlib.view.dialog.CommonDialog;
 import com.owner.R;
 import com.owner.identity.SelectIdActivity_;
@@ -24,42 +23,46 @@ public class UnIdifyDialog {
     /**
      * identityType :  "identityType": 0,//身份类型0个人1企业2联合
      * auditStatus :   0待审核1审核通过2审核未通过 3过期(和2未通过一样处理)-1未认证
+     * auditStatus为3，按照2驳回处理
      */
     private void unIdifyDialog(Context context, UserOwnerBean data) {
-        if (dialog != null && !dialog.isShowing()) {
-            dialog = null;
-        }
-        dialog = null;
-        if (dialog == null) {   //auditStatus为3，按照2驳回处理
-            String title, message, butText;
-            if (data.getAuditStatus() == 2 || data.getAuditStatus() == 3) {
-                title = "审核未通过";
-                message = data.getRemark();
-                butText = "去认证";
+
+        if (dialog != null) {
+            if (dialog.isShowing()) {
+                return;
             } else {
-                title = "认证提示";
-                message = StringIdentity.identityInfo(data) + "\n请您先认证信息";
-                butText = "去认证";
+                dialog.dismiss();
+                dialog = null;
             }
-            dialog = new CommonDialog.Builder(context)
-                    .setTitle(title)
-                    .setMessage(message)
-                    .setCancelButton(R.string.sm_cancel)
-                    .setConfirmButton(butText, (dialog12, which) -> {
-                        //当驳回时
-                        if (data.getAuditStatus() == 2 || data.getAuditStatus() == 3) {
-                            SelectIdActivity_.intent(context)
-                                    .isReject(true)
-                                    .rejectIdentityType(data.getIdentityType())
-                                    .start();
-                        } else { //当需要认证时
-                            SelectIdActivity_.intent(context).start();
-                        }
-                        dialog.dismiss();
-                        dialog = null;
-                    }).create();
-            dialog.showWithOutTouchable(false);
-            dialog.setCancelable(false);
         }
+        String title, message, butText;
+        if (data.getAuditStatus() == 2 || data.getAuditStatus() == 3) {
+            title = "审核未通过";
+            message = data.getRemark();
+            butText = "去认证";
+        } else {
+            title = "认证提示";
+            message = StringIdentity.identityInfo(data) + "\n请您先认证信息";
+            butText = "去认证";
+        }
+        dialog = new CommonDialog.Builder(context)
+                .setTitle(title)
+                .setMessage(message)
+                .setCancelButton(R.string.sm_cancel)
+                .setConfirmButton(butText, (dialog12, which) -> {
+                    //当驳回时
+                    if (data.getAuditStatus() == 2 || data.getAuditStatus() == 3) {
+                        SelectIdActivity_.intent(context)
+                                .isReject(true)
+                                .rejectIdentityType(data.getIdentityType())
+                                .start();
+                    } else { //当需要认证时
+                        SelectIdActivity_.intent(context).start();
+                    }
+                    dialog.dismiss();
+                    dialog = null;
+                }).create();
+        dialog.showWithOutTouchable(false);
+        dialog.setCancelable(false);
     }
 }
