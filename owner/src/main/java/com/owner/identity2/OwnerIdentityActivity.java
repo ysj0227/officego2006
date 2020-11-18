d5cd86a72977c5ce0bc040490b88412a5943d2f8
ysj0227 <1976290043@qq.com>
2020-11-18 18:08:32 +0800

    identity

--- owner/src/main/java/com/owner/identity2/OwnerIdentityActivity.java
+++ owner/src/main/java/com/owner/identity2/OwnerIdentityActivity.java
@@ -1,6 +1,9 @@
 package com.owner.identity2;
 
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
@@ -9,17 +12,31 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.core.content.FileProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.donkingliang.imageselector.utils.ImageSelector;
 import com.officego.commonlib.base.BaseMvpActivity;
+import com.officego.commonlib.common.SpUtils;
 import com.officego.commonlib.constant.Constants;
+import com.officego.commonlib.utils.FileHelper;
+import com.officego.commonlib.utils.FileUtils;
+import com.officego.commonlib.utils.ImageUtils;
+import com.officego.commonlib.utils.PermissionUtils;
+import com.officego.commonlib.utils.PhotoUtils;
 import com.officego.commonlib.utils.StatusBarUtils;
 import com.officego.commonlib.view.TitleBarView;
 import com.officego.commonlib.view.widget.SettingItemLayout;
+import com.owner.R;
 import com.owner.adapter.SearchAdapter;
+import com.owner.adapter.UploadCertImageAdapter;
+import com.owner.adapter.UploadLiceImageAdapter;
 import com.owner.dialog.IdentityTypeDialog;
 import com.owner.identity.model.IdentityBuildingBean;
+import com.owner.identity.model.ImageBean;
 import com.owner.identity2.contract.IdentityContract;
 import com.owner.identity2.presenter.IdentityPresenter;
 import com.owner.utils.CommUtils;
@@ -30,6 +47,7 @@ import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.OnActivityResult;
 import org.androidannotations.annotations.ViewById;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -41,10 +59,16 @@ import java.util.List;
 @EActivity(resName = "activity_owner_identity")
 public class OwnerIdentityActivity extends BaseMvpActivity<IdentityPresenter>
         implements IdentityContract.View, IdentityTypeDialog.IdentityTypeListener,
-        SearchAdapter.IdentityBuildingListener {
+        SearchAdapter.IdentityBuildingListener, UploadCertImageAdapter.UploadCertListener,
+        UploadLiceImageAdapter.UploadCertListener {
     private static final int REQUEST_GALLERY = 0xa0;
     private static final int REQUEST_CAMERA = 0xa1;
     private static final int REQUEST_CREATE_BUILDING = 0xa3;
+    private static final int TYPE_IDCARD_FRONT = 1;
+    private static final int TYPE_IDCARD_BACK = 2;
+    private static final int TYPE_CER = 3;//产证
+    private static final int TYPE_LICE = 4;//营业执照
+    private static final int TYPE_REN = 5;//补充材料
     @ViewById(resName = "title_bar")
     TitleBarView titleBar;
     @ViewById(resName = "tv_reject_reason")
@@ -71,12 +95,28 @@ public class OwnerIdentityActivity extends BaseMvpActivity<IdentityPresenter>
     View includeBusinessLicense;
     @ViewById(resName = "include_owner_personal_id")
     View includeOwnerPersonalId;
+    @ViewById(resName = "rv_property_ownership_certificate")
+    RecyclerView rvCertificate;
+    @ViewById(resName = "rv_business_licensee")
+    RecyclerView rvBusinessLicensee;
+    @ViewById(resName = "rv_additional_info")
+    RecyclerView rvRenlInfo;
 
     //是否展开
     private boolean isSpread;
     //搜索
     private SearchAdapter searchAdapter;
     private List<IdentityBuildingBean.DataBean> mList = new ArrayList<>();
+    //上传类型
+    private int mUploadType;
+    //本地路径
+    private String localIdCardFrontPath, localIdCardBackPath, localCerPath, localLicePath, localRenPath;
+    private Uri localPhotoUri;
+    //上传图片
+    private List<ImageBean> listCertificate = new ArrayList<>();
+    private List<ImageBean> listBusinessLice = new ArrayList<>();
+    private UploadCertImageAdapter cerAdapter;
+    private UploadLiceImageAdapter liceAdapter;
 
     @AfterViews
     void init() {
@@ -84,6 +124,7 @@ public class OwnerIdentityActivity extends BaseMvpActivity<IdentityPresenter>
         mPresenter = new IdentityPresenter();
         mPresenter.attachView(this);
         initViews();
+        initData();
     }
 
     private void initViews() {
@@ -91,6 +132,36 @@ public class OwnerIdentityActivity extends BaseMvpActivity<IdentityPresenter>
         LinearLayoutManager buildingManager = new LinearLayoutManager(context);
         rvRecommendBuilding.setLayoutManager(buildingManager);
         searchBuilding();
+        //上传产证图片
+        GridLayoutManager layoutManager1 = new GridLayoutManager(context, 3);
+        layoutManager1.setSmoothScrollbarEnabled(true);
+        layoutManager1.setAutoMeasureEnabled(true);
+        rvCertificate.setLayoutManager(layoutManager1);
+        rvCertificate.setNestedScrollingEnabled(false);
+        //营业执照
+        GridLayoutManager layoutManager2 = new GridLayoutManager(context, 3);
+        layoutManager2.setSmoothScrollbarEnabled(true);
+        layoutManager2.setAutoMeasureEnabled(true);
+        rvBusinessLicensee.setLayoutManager(layoutManager2);
+        rvBusinessLicensee.setNestedScrollingEnabled(false);
+    }
+
+    private void initData() {
+        //初始化本地图路径 产证
+        localCerPath = FileHelper.SDCARD_CACHE_IMAGE_PATH + SpUtils.getUserId() + "localCerPath.jpg";
+        //初始化图片默认添加一个
+        listCertificate.add(new ImageBean(false, 0, ""));
+        cerAdapter = new UploadCertImageAdapter(context, listCertificate);
+        cerAdapter.setListener(this);
+        rvCertificate.setAdapter(cerAdapter);
+
+        //初始化本地图路径 营业执照
+        localLicePath = FileHelper.SDCARD_CACHE_IMAGE_PATH + SpUtils.getUserId() + "localLicePath.jpg";
+        //初始化图片默认添加一个
+        listBusinessLice.add(new ImageBean(false, 0, ""));
+        liceAdapter = new UploadLiceImageAdapter(context, listBusinessLice);
+        liceAdapter.setListener(this);
+        rvBusinessLicensee.setAdapter(liceAdapter);
     }
 
     @Click(resName = "iv_expand")
@@ -230,4 +301,160 @@ public class OwnerIdentityActivity extends BaseMvpActivity<IdentityPresenter>
             includeOwnerPersonalId.setVisibility(View.GONE);
         }
     }
+
+    //房产证
+    @Override
+    public void addCertImage() {
+        mUploadType = TYPE_CER;
+        selectedDialog();
+    }
+
+    @Override
+    public void deleteCertImage(ImageBean bean, int position) {
+        listCertificate.remove(position);
+        cerAdapter.notifyDataSetChanged();
+    }
+
+    //营业执照
+    @Override
+    public void addLiceImage() {
+        mUploadType = TYPE_LICE;
+        selectedDialog();
+    }
+
+    @Override
+    public void deleteLiceImage(ImageBean bean, int position) {
+        listBusinessLice.remove(position);
+        liceAdapter.notifyDataSetChanged();
+    }
+
+    //房产认证拍照，相册
+    private void selectedDialog() {
+        final String[] items = {"拍照", "从相册选择"};
+        new AlertDialog.Builder(OwnerIdentityActivity.this)
+                .setItems(items, (dialogInterface, i) -> {
+                    if (i == 0) {
+                        takePhoto();
+                    } else if (i == 1) {
+                        openGallery();
+                    }
+                }).create().show();
+    }
+
+    //拍照
+    private void takePhoto() {
+        if (isOverLimit()) return;
+        if (!PermissionUtils.checkSDCardCameraPermission(this)) {
+            return;
+        }
+        if (!FileUtils.isSDExist()) {
+            shortTip(R.string.str_no_sd);
+            return;
+        }
+        File fileUri = null;
+        if (TYPE_CER == mUploadType) {
+            localCerPath = FileHelper.SDCARD_CACHE_IMAGE_PATH + System.currentTimeMillis() + "certificate.jpg";
+            fileUri = new File(localCerPath);
+        } else if (TYPE_LICE == mUploadType) {
+            localLicePath = FileHelper.SDCARD_CACHE_IMAGE_PATH + System.currentTimeMillis() + "businessLice.jpg";
+            fileUri = new File(localLicePath);
+        }
+        localPhotoUri = Uri.fromFile(fileUri);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            //通过FileProvider创建一个content类型的Uri
+            localPhotoUri = FileProvider.getUriForFile(this, Constants.FILE_PROVIDER_AUTHORITY, fileUri);
+        }
+        PhotoUtils.takePicture(this, localPhotoUri, REQUEST_CAMERA);
+    }
+
+    private boolean isOverLimit() {
+        if (TYPE_CER == mUploadType) {//房产证
+            if (listCertificate.size() >= 10) {
+                shortTip(R.string.tip_image_upload_overlimit);
+                return true;
+            }
+        } else if (TYPE_LICE == mUploadType) {//营业执照
+            if (listBusinessLice.size() >= 10) {
+                shortTip(R.string.tip_image_upload_overlimit);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int num() {
+        int num;
+        if (TYPE_CER == mUploadType) {//房产证
+            num = 10 - listCertificate.size();
+        } else if (TYPE_LICE == mUploadType) {//营业执照
+            num = 10 - listBusinessLice.size();
+        } else {
+            num = 10;
+        }
+        return num;
+    }
+
+    private void openGallery() {
+        if (!PermissionUtils.checkStoragePermission(this)) {
+            return;
+        }
+        //是否上传房产证
+        if (TYPE_CER == mUploadType || TYPE_LICE == mUploadType) {
+            if (isOverLimit()) return;
+            ImageSelector.builder()
+                    .useCamera(false) // 设置是否使用拍照
+                    .setSingle(false)  //设置是否单选
+                    .setMaxSelectCount(num())
+                    .canPreview(true) //是否可以预览图片，默认为true
+                    .start(this, REQUEST_GALLERY); // 打开相册
+        } else {
+            ImageSelector.builder()
+                    .useCamera(false) // 设置是否使用拍照
+                    .setSingle(true)  //设置是否单选
+                    .canPreview(true) //是否可以预览图片，默认为true
+                    .start(this, REQUEST_GALLERY); // 打开相册
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_CAMERA) {//拍照
+                if (TYPE_CER == mUploadType) {//房产证
+                    ImageUtils.isSaveCropImageView(localCerPath);
+                    listCertificate.add(listCertificate.size() - 1, new ImageBean(false, 0, localCerPath));
+                    cerAdapter.notifyDataSetChanged();
+                } else if (TYPE_LICE == mUploadType) {//营业执照
+                    ImageUtils.isSaveCropImageView(localLicePath);
+                    listBusinessLice.add(listBusinessLice.size() - 1, new ImageBean(false, 0, localLicePath));
+                    liceAdapter.notifyDataSetChanged();
+                }
+
+            } else if (requestCode == REQUEST_GALLERY && data != null) {//相册
+                List<String> images = data.getStringArrayListExtra(ImageSelector.SELECT_RESULT);
+                if (TYPE_CER == mUploadType) {//房产证
+                    for (int i = 0; i < images.size(); i++) {
+                        ImageUtils.isSaveCropImageView(images.get(i));
+                        listCertificate.add(listCertificate.size() - 1, new ImageBean(false, 0, images.get(i)));
+                    }
+                    cerAdapter.notifyDataSetChanged();
+                } else if (TYPE_LICE == mUploadType) {//营业执照
+                    for (int i = 0; i < images.size(); i++) {
+                        ImageUtils.isSaveCropImageView(images.get(i));
+                        listBusinessLice.add(listBusinessLice.size() - 1, new ImageBean(false, 0, images.get(i)));
+                    }
+                    liceAdapter.notifyDataSetChanged();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        PermissionUtils.requestPermissions(context, requestCode, permissions, grantResults);
+    }
+
 }
