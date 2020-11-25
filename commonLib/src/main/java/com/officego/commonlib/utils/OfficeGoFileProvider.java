73f46cd9093b6fde5a71e3308a97af544a629660
ysj0227 <1976290043@qq.com>
2020-11-25 17:29:12 +0800

    identity

---
+++ commonLib/src/main/java/com/officego/commonlib/utils/OfficeGoFileProvider.java
@@ -0,0 +1,10 @@
+package com.officego.commonlib.utils;
+
+import androidx.core.content.FileProvider;
+
+/**
+ * Created by shijie
+ * Date 2020/11/25
+ **/
+public class OfficeGoFileProvider extends FileProvider {
+}
