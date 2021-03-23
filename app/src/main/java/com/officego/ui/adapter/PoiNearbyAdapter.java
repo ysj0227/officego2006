3cb9aa5ba8a01ff05c07ffab3c9b3739003e5ae3
ysj0227 <1976290043@qq.com>
2021-03-23 10:15:09 +0800

    fix

--- app/src/main/java/com/officego/ui/adapter/PoiNearbyAdapter.java
+++ app/src/main/java/com/officego/ui/adapter/PoiNearbyAdapter.java
@@ -1,6 +1,7 @@
 package com.officego.ui.adapter;
 
 import android.content.Context;
+import android.view.View;
 import android.widget.TextView;
 
 import com.amap.api.services.core.PoiItem;
@@ -28,16 +29,21 @@ public class PoiNearbyAdapter extends CommonListAdapter<PoiItem> {
         void poiItemOnClick(PoiItem data);
     }
 
+    private final List<PoiItem> list;
+
     public PoiNearbyAdapter(Context context, List<PoiItem> list) {
         super(context, R.layout.item_poi_nearby_name, list);
+        this.list = list;
     }
 
     @Override
     public void convert(ViewHolder holder, final PoiItem bean) {
         TextView tvName = holder.getView(R.id.tv_name);
         TextView tvAddress = holder.getView(R.id.tv_address);
+        View vLine = holder.getView(R.id.v_line);
         tvName.setText(bean.getTitle());
         tvAddress.setText(String.format("| %s%s", bean.getAdName(), bean.getSnippet()));
+        vLine.setVisibility(holder.getAdapterPosition() != list.size() - 1 ? View.VISIBLE : View.GONE);
         holder.itemView.setOnClickListener(view -> {
             if (listener != null) {
                 listener.poiItemOnClick(bean);
